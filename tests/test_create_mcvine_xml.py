db6d9f8df42f73bf46ac77298bf7cc7156e658f0
Jiao Lin <linjiao@ornl.gov>
2018-06-16 07:48:15 -0400

    slight refactor of test_create_mcvine_xml.py, adding mantid2mcvine.testutils

--- tests/test_create_mcvine_xml.py
+++ tests/test_create_mcvine_xml.py
@@ -21,36 +21,11 @@ class Test(unittest.TestCase):
             ds_shape = detsys_shape, tube_info=tube_info,
             xmloutput=out,
         )
-        this = load(out)
-        expected = load(os.path.join(here, 'expected/mcvine.xml'))
-        self.assert_(elements_equal(this, expected))
+        expected = os.path.join(here, 'expected/mcvine.xml')
+        from mantid2mcvine.testutils import xml_equal
+        self.assert_(xml_equal(out, expected))
         return
 
-
-def load(path):
-    import xml.etree.ElementTree as ET
-    tree = ET.parse(path)
-    root = tree.getroot()
-    return root
-
-def elements_equal(e1, e2):
-    if e1.tag != e2.tag:
-        print "tag: %r != %r" % (e1.tag, e2.tag)
-        return False
-    _strip = lambda v: (v or '').strip() # deal with None
-    if _strip(e1.text) != _strip(e2.text):
-        print "text: %r != %r" % (e1.text, e2.text)
-        return False
-    if _strip(e1.tail) != _strip(e2.tail):
-        print "tail: %r != %r" % (e1.tail, e2.tail)
-        return False
-    if e1.attrib != e2.attrib:
-        print "attrib: %r != %r" % (e1.attrib, e2.attrib)
-        return False
-    if len(e1) != len(e2):
-        print "len: %r != %r" % (len(e1), len(e2))
-        return False
-    return all(elements_equal(c1, c2) for c1, c2 in zip(e1, e2))
 
 if __name__ =='__main__': unittest.main()
 
