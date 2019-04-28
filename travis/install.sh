d1b53e06814e2a99160596bdaa11909e6116f43e
Jiao Lin <linjiao@ornl.gov>
2019-04-28 12:38:19 -0400

    fixing travis: battle output-size limit and no-output time-limit

---
+++ travis/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+export GIT_FULL_HASH=`git rev-parse HEAD`
+conda config --set always_yes true
+conda update conda
+conda config --add channels conda-forge
+conda config --add channels diffpy
+conda config --add channels mantid
+conda config --add channels mcvine
+conda create -n testenv pip pytest pytest-cov python=$TRAVIS_PYTHON_VERSION
+source activate testenv
+#conda install -n testenv numpy 
+conda install mpich
+conda install -c mcvine/label/unstable mcvine-core
+conda install -c mcvine/label/unstable mantid-framework=3.13 muparser=2.2.5=0
+python -c "import matplotlib; import mantid"
+mcvine
+python setup.py install
+pip install coveralls
