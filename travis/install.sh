3b17e761e97964f473c4edcc6c41b61ca25fe4e9
Jiao Lin <jiao.lin@gmail.com>
2020-09-22 10:42:08 -0400

    travis: build conda pkg and upload

--- travis/install.sh
+++ travis/install.sh
@@ -10,10 +10,6 @@ conda config --add channels mantid
 conda config --add channels mcvine
 conda create -n testenv python=$TRAVIS_PYTHON_VERSION
 source activate testenv
-#conda install -n testenv numpy 
-# conda install mpich
-# conda install -c mcvine/label/unstable mantid-framework=3.13 muparser=2.2.5=0
-# conda install mantid-framework=4
 conda install -c mcvine/label/unstable pytest pytest-cov coveralls mpich mcvine-core mantid-framework=4
 python -c "import matplotlib; import mantid"
 mcvine
