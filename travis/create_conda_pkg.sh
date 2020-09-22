a67e69e9a2db70ccc63bd7351d9039e4629ea90f
Jiao Lin <jiao.lin@gmail.com>
2020-09-22 12:34:26 -0400

    fixing conda upload

--- travis/create_conda_pkg.sh
+++ travis/create_conda_pkg.sh
@@ -7,4 +7,4 @@ cd travis
 sed -e "s|XXXVERSIONXXX|$_CONDA_PKG_VER_|g" meta.yaml.template | sed -e "s|XXXGIT_REVXXX|$GIT_REV|g" > meta.yaml
 cat meta.yaml
 conda build --python=$TRAVIS_PYTHON_VERSION .
-anaconda -t $CONDA_UPLOAD_TOKEN upload --force /home/travis/mc/conda-bld/linux-64/mantid2mcvine-$_CONDA_PKG_VER_-*.tar.bz2 --label unstable
+anaconda -t $CONDA_UPLOAD_TOKEN upload --force /home/travis/mc/conda-bld/noarch/mantid2mcvine-$_CONDA_PKG_VER_-*.tar.bz2 --label unstable
