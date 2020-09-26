7ae977d38640646d6ba4df1e029aff246a4a7f44
Jiao Lin <jiao.lin@gmail.com>
2020-09-25 21:39:14 -0400

    travis: trying to solve the conda resolving problem in create conda package

--- travis/install.sh
+++ travis/install.sh
@@ -8,9 +8,10 @@ conda config --add channels conda-forge
 conda config --add channels diffpy
 conda config --add channels mantid
 conda config --add channels mcvine
+conda config --add channels mcvine/label/unstable
 conda create -n testenv python=$TRAVIS_PYTHON_VERSION
 source activate testenv
-conda install -c mcvine/label/unstable pytest pytest-cov coveralls mpich mcvine-core mantid-framework=4
+conda install -c pytest pytest-cov coveralls mpich mcvine-core mantid-framework=4
 python -c "import matplotlib; import mantid"
 mcvine
 python setup.py install
