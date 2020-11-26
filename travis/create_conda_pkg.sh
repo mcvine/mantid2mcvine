785a82ff541b39ac706375ad0b6bd14125870a38
Jiao Lin <jiao.lin@gmail.com>
2020-11-26 07:42:03 -0800

    debugging anaconda upload problem

--- travis/create_conda_pkg.sh
+++ travis/create_conda_pkg.sh
@@ -5,10 +5,14 @@ set -x
 
 conda install -n root conda-build
 conda install anaconda-client
+which anaconda
 conda config --set anaconda_upload no
 
+# build
 cd travis
 sed -e "s|XXXVERSIONXXX|$_CONDA_PKG_VER_|g" meta.yaml.template | sed -e "s|XXXGIT_REVXXX|$GIT_REV|g" > meta.yaml
 cat meta.yaml
 conda build --python=$TRAVIS_PYTHON_VERSION .
+
+# upload
 anaconda -t $CONDA_UPLOAD_TOKEN upload --force /home/travis/mc/conda-bld/noarch/mantid2mcvine-$_CONDA_PKG_VER_-*.tar.bz2 --label unstable
