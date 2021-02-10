4af7cbae9fa1c650b6297e83f816c63957cef319
Jiao Lin <jiao.lin@gmail.com>
2021-02-10 07:43:03 -0800

    debugging

--- travis/create_conda_pkg.sh
+++ travis/create_conda_pkg.sh
@@ -5,17 +5,18 @@ set -x
 
 conda config --show channels
 conda install -n root conda-build
-conda install anaconda-client
-which anaconda
-conda config --set anaconda_upload no
+echo "create_conda_pkg.sh done"
+#conda install anaconda-client
+#which anaconda
+#conda config --set anaconda_upload no
 
 # build
-cd travis
-sed -e "s|XXXVERSIONXXX|$_CONDA_PKG_VER_|g" meta.yaml.template | sed -e "s|XXXGIT_REVXXX|$GIT_REV|g" > meta.yaml
-cat meta.yaml
-conda build --python=$TRAVIS_PYTHON_VERSION .
+#cd travis
+#sed -e "s|XXXVERSIONXXX|$_CONDA_PKG_VER_|g" meta.yaml.template | sed -e "s|XXXGIT_REVXXX|$GIT_REV|g" > meta.yaml
+#cat meta.yaml
+#conda build --python=$TRAVIS_PYTHON_VERSION .
 
 # upload
-CONDA_ROOT_PREFIX=$(realpath $(dirname `which conda`)/..)
-anaconda -t $CONDA_UPLOAD_TOKEN upload --force --label unstable \
-         $CONDA_ROOT_PREFIX/conda-bld/noarch/mantid2mcvine-$_CONDA_PKG_VER_-*.tar.bz2
+#CONDA_ROOT_PREFIX=$(realpath $(dirname `which conda`)/..)
+#anaconda -t $CONDA_UPLOAD_TOKEN upload --force --label unstable \
+#         $CONDA_ROOT_PREFIX/conda-bld/noarch/mantid2mcvine-$_CONDA_PKG_VER_-*.tar.bz2
