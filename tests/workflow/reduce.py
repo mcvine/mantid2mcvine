9a5279880161774d81f664e8c6f156e480d1ea9b
Jiao Lin <jiao.lin@gmail.com>
2023-03-22 19:35:48 -0700

    msa.setFacility -> mantid.kernel.config.setFacility:   tests/workflow/reduce.py

--- tests/workflow/reduce.py
+++ tests/workflow/reduce.py
@@ -1,12 +1,12 @@
+import mantid
 from mantid import simpleapi as msa, mtd
-
 
 def reduce(path):
     ws = msa.Load(path)
     ws1 = msa.CropWorkspace(InputWorkspace=ws, OutputWorkspace='ws1', StartWorkspaceIndex=1)
     old_facility = msa.config.getFacility().name()
     try:
-        msa.setFacility("SNS")  # DgsReduction requires SNS as default facility
+        mantid.kernel.config.setFacility("SNS")  # DgsReduction requires SNS as default facility
         msa.DgsReduction(
             SampleInputWorkspace=ws1,
             OutputWorkspace='reduced',
@@ -15,7 +15,7 @@ def reduce(path):
             SofPhiEIsDistribution=1, IncidentBeamNormalisation='ByCurrent',
             CorrectKiKf=1, TimeZeroGuess=20)
     finally:
-        msa.setFacility(old_facility)  # restore the old facility
+        mantid.kernel.config.setFacility(old_facility)  # restore the old facility
     reduced = mtd['reduced']
     from mcvine.instruments.ARCS.applications.nxs import getSqeHistogramFromMantidWS
     getSqeHistogramFromMantidWS(reduced, 'iqe.h5', qaxis=(0, 0.05, 3.5))
