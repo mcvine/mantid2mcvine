ac29541e753597d2204e9920894c8a53dbe6ae05
Jose Borreguero <borreguero@gmail.com>
2023-03-22 20:07:03 -0400

    Temporarily amend the default facility
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- tests/workflow/reduce.py
+++ tests/workflow/reduce.py
@@ -1,18 +1,26 @@
+from mantid import simpleapi as msa, mtd
+
+
 def reduce(path):
-    from mantid import simpleapi as msa, mtd
     ws = msa.Load(path)
     ws1 = msa.CropWorkspace(InputWorkspace=ws, OutputWorkspace='ws1', StartWorkspaceIndex=1)
-    msa.DgsReduction(
-        SampleInputWorkspace=ws1,
-        OutputWorkspace='reduced',
-        IncidentEnergyGuess=70., UseIncidentEnergyGuess=1,
-        EnergyTransferRange = [-70, 0.5, 65],
-        SofPhiEIsDistribution=1, IncidentBeamNormalisation='ByCurrent',
-        CorrectKiKf=1, TimeZeroGuess=20)
+    old_facility = msa.config.getFacility().name()
+    try:
+        msa.setFacility("SNS")  # DgsReduction requires SNS as default facility
+        msa.DgsReduction(
+            SampleInputWorkspace=ws1,
+            OutputWorkspace='reduced',
+            IncidentEnergyGuess=70., UseIncidentEnergyGuess=1,
+            EnergyTransferRange=[-70, 0.5, 65],
+            SofPhiEIsDistribution=1, IncidentBeamNormalisation='ByCurrent',
+            CorrectKiKf=1, TimeZeroGuess=20)
+    finally:
+        msa.setFacility(old_facility)  # restore the old facility
     reduced = mtd['reduced']
     from mcvine.instruments.ARCS.applications.nxs import getSqeHistogramFromMantidWS
     getSqeHistogramFromMantidWS(reduced, 'iqe.h5', qaxis=(0, 0.05, 3.5))
     return
+
 
 if __name__ == '__main__':
     import sys
